84cde56724be59f198716a008af812e9c933953e
Faysal Ahammed <faysal024012@gmail.com>
2023-08-29 13:59:11 +0600

    create keystore file during job execution

--- build-config.sh
+++ build-config.sh
@@ -2,6 +2,7 @@
 
 echo "-------- generating build config--------"
 
+echo -n ${KEYSTORE_FILE} | base64 -d > ./android/app/brettonwoods-gold.keystore
 cat <<EOT >> ./android/gradle.properties
 
 KEY_ALIAS=$KEY_ALIAS
