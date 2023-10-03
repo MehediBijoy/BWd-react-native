64529663e21a76b317f87899ed292a52bf22282a
Mehedi <mehedibubt34@gmail.com>
2023-10-03 16:22:17 +0600

    added copy button

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,10 @@
 import type {ReactNode} from 'react'
+import type {Colors as RNColors} from '@rneui/themed'
 
 export type Children = {
   children: ReactNode
 }
 
 export type SetErrorKey = `root.${string}`
+
+export type Colors = keyof Omit<RNColors, 'platform'>
