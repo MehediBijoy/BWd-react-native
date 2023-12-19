912c610f753a8e7d9491c3394eb01ba3f64a2537
Khalid7487 <khalid15-7487@diu.edu.bd>
2023-12-19 14:51:51 +0600

    add warnign modal for wallect connections

--- src/navigators/WalletButton/WalletButton.tsx
+++ src/navigators/WalletButton/WalletButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button} from '@rneui/themed'
+import {Button, Dialog, Text} from '@rneui/themed'
 import {useTranslation} from 'react-i18next'
 import {useMutation} from '@tanstack/react-query'
 import {useWalletConnectModal} from '@walletconnect/modal-react-native'
@@ -9,9 +9,28 @@ import {shortAddress} from 'utils'
 import {useProfile} from 'hooks/helper'
 import {useWalletController} from 'hooks/states'
 
+type AlertModalProps = {
+  isVisible: boolean
+  onClose: () => void
+  onPress: () => void
+}
+
+const AlertModal = ({isVisible, onClose, onPress}: AlertModalProps) => {
+  const {t} = useTranslation()
+  return (
+    <Dialog isVisible={isVisible} onBackdropPress={onClose}>
+      <Text style={{fontSize: 16}}>{t('walletConnect.alertMessage')}</Text>
+      <Dialog.Actions>
+        <Dialog.Button title='OK' onPress={onPress} />
+      </Dialog.Actions>
+    </Dialog>
+  )
+}
+
 const WalletButton = () => {
   const api = useApi()
   const {t} = useTranslation()
+  const [visible, setVisible] = React.useState(false)
   const {profile, setProfile} = useProfile()
   const {setIsOpened} = useWalletController()
   const {open, isOpen, address, isConnected} = useWalletConnectModal()
@@ -30,13 +49,24 @@ const WalletButton = () => {
   }, [isConnected, address, profile, mutate])
 
   return (
-    <Button
-      loading={isOpen}
-      onPress={() => connect()}
-      titleStyle={{fontSize: 14}}
-      title={isConnected && address ? shortAddress(address) : t('walletConnect.connectWallet')}
-      containerStyle={{minWidth: 120, borderRadius: 8}}
-    />
+    <>
+      <Button
+        loading={isOpen}
+        // onPress={() => connect()}
+        onPress={() => (!isConnected ? setVisible(true) : connect())}
+        titleStyle={{fontSize: 14}}
+        title={isConnected && address ? shortAddress(address) : t('walletConnect.connectWallet')}
+        containerStyle={{minWidth: 120, borderRadius: 8}}
+      />
+      <AlertModal
+        isVisible={visible}
+        onClose={() => setVisible(false)}
+        onPress={() => {
+          setVisible(false)
+          connect()
+        }}
+      />
+    </>
   )
 }
 
